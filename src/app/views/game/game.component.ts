5d5737aad75dffede07be3d6cde733fd21922013
ankitzpro <mankit322@gmail.com>
2020-06-23 20:18:22 +0530

    3 files modified

--- src/app/views/game/game.component.ts
+++ src/app/views/game/game.component.ts
@@ -19,28 +19,59 @@ export class GameComponent  {
   level=this.service.level+1;
 
   arr=['triangle','square','circle','cross'];
-  ansarr=[];
+  //ansarr=[];
   arr2=['triangle','square','circle','cross'];
+  ansar1=[];
+  ansar2=[];
+  ansar3=[];
+  randpos:number;
   ngOnInit() {
- console.log('Hello');
     this.arr=this.shuffle(this.arr);
-    console.log(this.arr);
     this.arr2=this.shuffle(this.arr2);
-    console.log(this.arr2);
     
 
     for(var i=0;i<this.arr.length;i++){ 
       for(var j=0;j<this.arr2.length;j++){
         
         if(this.arr[i]==this.arr2[j]){
-          this.ansarr.push(j+1);
+          this.ansar1.push(j+1);
+          this.ansar2.push(j+1);
+          this.ansar3.push(j+1);
         }
       }
     }
-    console.log(this.ansarr);
-    console.log(this.arr);
-    console.log(this.arr2);
-  }
+
+   console.log(this.ansar1);
+   console.log(this.ansar2);
+   console.log(this.ansar3);
+
+this.randpos = Math.floor(Math.random() * 3)+1;
+
+   console.log(this.randpos);
+switch(this.randpos){
+ case 1:
+ {
+   this.ansar2=this.shuffle(this.ansar2);
+   this.ansar3=this.shuffle(this.ansar3);
+   break;
+ }
+ case 2:
+ {
+   this.ansar1=this.shuffle(this.ansar1);
+   this.ansar3=this.shuffle(this.ansar3);}
+   break;
+ case 3:
+ {
+   this.ansar2=this.shuffle(this.ansar2);
+   this.ansar1=this.shuffle(this.ansar1);
+   break;
+   }
+   }
+   console.log(this.ansar1);
+   console.log(this.ansar2);
+   console.log(this.ansar3);
+}
+  
 
    shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
